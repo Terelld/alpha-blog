aefe70e523c08611057787070b3ecda60278442b
Terell Davy <terell.davy@ramseysolutions.com>
2024-06-20 09:33:14 -0500

    show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
-
+  
 
   get "up" => "rails/health#show", as: :rails_health_check
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_17_202101) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_19_154641) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
